d63590a6bf7225ffe1499566fdfe55112ca5349d
Snow Yáng <snow.yang@fluv.com>
2024-08-22 18:45:50 +0800

    Finish badge

---
+++ swift-ui/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  swift-ui
+//
+//  Created by S on 2024/8/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 7))
+}
