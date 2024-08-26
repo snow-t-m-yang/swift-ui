83508c9555b35dc57e26e8c6b513985bb55a30a9
Snow Yáng <snow.yang@fluv.com>
2024-08-26 18:59:35 +0800

    Create a category list

--- swift-ui/Modal/Landmark.swift
+++ swift-ui/Modal/Landmark.swift
@@ -23,6 +23,14 @@ struct Landmark: Hashable, Codable, Identifiable {
     //		Image(imageName)
     //	}
 
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+        case buldings = "Buildings"
+    }
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
